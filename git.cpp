e380b933d871aeba7398067efe864088a6dd36b4
fatma-mostafa1 <fatmamostfa518@gmail.com>
2024-12-23 04:04:06 +0200

    Add files via upload

---
+++ git.cpp
@@ -0,0 +1,21 @@
+#include<iostrea>
+#using namespace std;
+
+int sum_m(int a, int b);
+void main()
+{
+	cout << "fatamaaaa";
+	cout << " gghhuhiubi";
+
+	cout << "fatama";
+	cout << " gghhuhii";
+	cout << "fatma of a77"
+	cout << sum_m(3, 2);
+
+
+}
+int sum_m(int a, int b)
+{
+	return a + b;
+
+}
