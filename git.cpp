83b2c20dfc7494da88ad2696bc9836207af42436
fatma <fatmamostfa518@gmail.com>
2024-12-23 03:15:51 +0200

    done

--- git.cpp
+++ git.cpp
@@ -6,18 +6,13 @@ void main()
 {
 	cout << "fatamaaaa";
 	cout << " gghhuhiubi";
-<<<<<<< HEAD
-	cout << " ggh";
 
-	cout << sum(3,5);
-=======
 	cout << "fatama";
 	cout << " gghhuhii";
 
 	cout << sum_m(3, 2);
 
 
->>>>>>> tm
 }
 int sum_m(int a, int b)
 {
