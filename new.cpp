3d37627acf44290dfbcf949b0009d0ee1cb13158
fatma <fatmamostfa518@gmail.com>
2024-12-23 04:08:49 +0200

    init add

---
+++ new.cpp
@@ -0,0 +1,15 @@
+#include<iostrea>
+#using namespace std;
+
+void main()
+{
+	cout << "fatamaaaa";
+	cout << " gghhuhiubi";
+
+	cout << "fatama";
+	cout << " gghhuhii";
+	cout << "fatma of a77"
+		cout << sum_m(3, 2);
+
+
+}
